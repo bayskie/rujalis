c3daf23520fa7d92b12290de11fca85e9d09766a
Bayu Rizky Kurnia Pratama <bayurkp.dev@gmail.com>
2025-05-21 14:29:59 +0800

    chore: add react router

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-export default function App() {
+export default function Home() {
   return (
     <>
       <h1 className="text-3xl">Hello world!</h1>
