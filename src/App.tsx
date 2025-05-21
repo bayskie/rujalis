bf9ccbd9819406e299b4ed5b546caae830e820d3
Bayu Rizky Kurnia Pratama <bayurkp.dev@gmail.com>
2025-05-21 14:14:08 +0800

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl">Hello world!</h1>
+      <Button>Click Me!</Button>
+    </>
+  );
+}
